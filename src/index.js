73d9a580aea7a3114e579376713f8ebeadf81dbe
ANDRUXUIS <abdealex05@gmail.com>
2021-01-10 23:56:48 +0100

    Added to deploy to heroku

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const nodemailer = require("nodemailer");
 
 const app = express();
 app.use(express.json());
-app.get("/", (req, res) => {
+app.post("/", (req, res) => {
   // create reusable transporter object using the default SMTP transport
   const transporter = nodemailer.createTransport({
     host: "smtp.elasticemail.com",
