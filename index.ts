bc0a4b254e0d8212c9bd60885c6f8830601d8fea
ANDRUXUIS <abdealex05@gmail.com>
2021-01-10 22:53:06 +0100

    Added to deploy to heroku

--- index.ts
+++ index.ts
@@ -1,49 +1,194 @@
-import * as functions from "firebase-functions"
-import * as nodemailer from "nodemailer"
-
-export const sendEmail = functions.https.onRequest((req, res) => {
-  interface Payload {
-    username: string
-    email: string
-    subject: string
-    message: string
-  }
-  // async..await is not allowed in global scope, must use a wrapper
-  async function main() {
-    // create reusable transporter object using the default SMTP transport
-    const transporter = nodemailer.createTransport({
-      host: "smtp.elasticemail.com",
-      port: 2525,
-      secure: false, // true for 465, false for other ports
-      auth: {
-        user: "abdearrahmaneouali05@gmail.com", // generated ethereal user
-        pass: "EBF64AA8C493ABDDB9C0BA7E63803F70E186", // generated ethereal password
-      },
+"use strict"
+var __awaiter =
+  (this && this.__awaiter) ||
+  function (thisArg, _arguments, P, generator) {
+    function adopt(value) {
+      return value instanceof P
+        ? value
+        : new P(function (resolve) {
+            resolve(value)
+          })
+    }
+    return new (P || (P = Promise))(function (resolve, reject) {
+      function fulfilled(value) {
+        try {
+          step(generator.next(value))
+        } catch (e) {
+          reject(e)
+        }
+      }
+      function rejected(value) {
+        try {
+          step(generator["throw"](value))
+        } catch (e) {
+          reject(e)
+        }
+      }
+      function step(result) {
+        result.done
+          ? resolve(result.value)
+          : adopt(result.value).then(fulfilled, rejected)
+      }
+      step((generator = generator.apply(thisArg, _arguments || [])).next())
     })
-    const payload: Payload = req.body
-    payload.message = `
-     From: ${payload.email}
-      <br/>
-      Message:
-        ${payload.message}
-    `
-    console.log(payload)
-    try {
-      // send mail with defined transport object
-      const info = await transporter.sendMail({
-        from: "abdearrahmaneouali05@gmail.com", // sender address
-        to: "alexabde01@gmail.com", // list of receivers
-        subject: payload.subject, // Subject line
-        text: "", // plain text body
-        html: `<b>${payload.message}</b>`, // html body
-      })
-
-      res.send(`Message sent: ${info.messageId}`)
-      // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-    } catch (error) {
-      res.status(500).send("Error has occurred")
+  }
+var __generator =
+  (this && this.__generator) ||
+  function (thisArg, body) {
+    var _ = {
+        label: 0,
+        sent: function () {
+          if (t[0] & 1) throw t[1]
+          return t[1]
+        },
+        trys: [],
+        ops: [],
+      },
+      f,
+      y,
+      t,
+      g
+    return (
+      (g = { next: verb(0), throw: verb(1), return: verb(2) }),
+      typeof Symbol === "function" &&
+        (g[Symbol.iterator] = function () {
+          return this
+        }),
+      g
+    )
+    function verb(n) {
+      return function (v) {
+        return step([n, v])
+      }
+    }
+    function step(op) {
+      if (f) throw new TypeError("Generator is already executing.")
+      while (_)
+        try {
+          if (
+            ((f = 1),
+            y &&
+              (t =
+                op[0] & 2
+                  ? y["return"]
+                  : op[0]
+                  ? y["throw"] || ((t = y["return"]) && t.call(y), 0)
+                  : y.next) &&
+              !(t = t.call(y, op[1])).done)
+          )
+            return t
+          if (((y = 0), t)) op = [op[0] & 2, t.value]
+          switch (op[0]) {
+            case 0:
+            case 1:
+              t = op
+              break
+            case 4:
+              _.label++
+              return { value: op[1], done: false }
+            case 5:
+              _.label++
+              y = op[1]
+              op = [0]
+              continue
+            case 7:
+              op = _.ops.pop()
+              _.trys.pop()
+              continue
+            default:
+              if (
+                !((t = _.trys), (t = t.length > 0 && t[t.length - 1])) &&
+                (op[0] === 6 || op[0] === 2)
+              ) {
+                _ = 0
+                continue
+              }
+              if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) {
+                _.label = op[1]
+                break
+              }
+              if (op[0] === 6 && _.label < t[1]) {
+                _.label = t[1]
+                t = op
+                break
+              }
+              if (t && _.label < t[2]) {
+                _.label = t[2]
+                _.ops.push(op)
+                break
+              }
+              if (t[2]) _.ops.pop()
+              _.trys.pop()
+              continue
+          }
+          op = body.call(thisArg, _)
+        } catch (e) {
+          op = [6, e]
+          y = 0
+        } finally {
+          f = t = 0
+        }
+      if (op[0] & 5) throw op[1]
+      return { value: op[0] ? op[1] : void 0, done: true }
     }
   }
-
-  main().catch(e => console.log(e))
+exports.__esModule = true
+exports.sendEmail = void 0
+var functions = require("firebase-functions")
+var nodemailer = require("nodemailer")
+exports.sendEmail = functions.https.onRequest(function (req, res) {
+  // async..await is not allowed in global scope, must use a wrapper
+  function main() {
+    return __awaiter(this, void 0, void 0, function () {
+      var transporter, payload, info, error_1
+      return __generator(this, function (_a) {
+        switch (_a.label) {
+          case 0:
+            transporter = nodemailer.createTransport({
+              host: "smtp.elasticemail.com",
+              port: 2525,
+              secure: false,
+              auth: {
+                user: "abdearrahmaneouali05@gmail.com",
+                pass: "EBF64AA8C493ABDDB9C0BA7E63803F70E186",
+              },
+            })
+            payload = req.body
+            payload.message =
+              "\n     From: " +
+              payload.email +
+              "\n      <br/>\n      Message:\n        " +
+              payload.message +
+              "\n    "
+            console.log(payload)
+            _a.label = 1
+          case 1:
+            _a.trys.push([1, 3, , 4])
+            return [
+              4 /*yield*/,
+              transporter.sendMail({
+                from: "abdearrahmaneouali05@gmail.com",
+                to: "alexabde01@gmail.com",
+                subject: payload.subject,
+                text: "",
+                html: "<b>" + payload.message + "</b>",
+              }),
+            ]
+          case 2:
+            info = _a.sent()
+            res.send("Message sent: " + info.messageId)
+            return [3 /*break*/, 4]
+          case 3:
+            error_1 = _a.sent()
+            res.status(500).send("Error has occurred")
+            return [3 /*break*/, 4]
+          case 4:
+            return [2 /*return*/]
+        }
+      })
+    })
+  }
+  main()["catch"](function (e) {
+    return console.log(e)
+  })
 })
