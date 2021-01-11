892a03b38fa23a68edc6355dfb3a2a270e26c57d
ANDRUXUIS <abdealex05@gmail.com>
2021-01-11 17:42:51 +0100

    Added to deploy to heroku

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
   res.header(
-    "Access-Control-Allow-Headers",
+    "Access-Control-Request-Headers",
     "access-control-allow-origin,content-type"
   );
   next();
