cc47da55fa1c13aef17e58ef799e8deec7a6a2ca
ANDRUXUIS <abdealex05@gmail.com>
2021-01-11 22:40:45 +0100

    Added to deploy to heroku

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 const app = express();
 app.use(express.json());
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", process.env.HOST_SITE);
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
