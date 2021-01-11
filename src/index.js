283991bbf61ba7508a00f1a418909ac76fb17e4f
ANDRUXUIS <abdealex05@gmail.com>
2021-01-11 17:16:32 +0100

    Added to deploy to heroku

--- src/index.js
+++ src/index.js
@@ -5,10 +5,19 @@ const cors = require("cors");
 
 
 const app = express();
+app.use(cors());
+
+app.options("*", cors());
+var allowCrossDomain = function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+};
+app.use(allowCrossDomain);
 app.use(express.json());
 
-
-app.post("/", cors(), (req, res) => {
+app.post("/", (req, res) => {
   // create reusable transporter object using the default SMTP transport
   const transporter = nodemailer.createTransport({
     host: "smtp.elasticemail.com",
