9170ff41bc693663be79fa27c4d20de94e88c648
ANDRUXUIS <abdealex05@gmail.com>
2021-01-11 17:27:45 +0100

    Added to deploy to heroku

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const nodemailer = require("nodemailer");
-const cors = require("cors");
+// const cors = require("cors");
 
 
 
 const app = express();
-app.use(cors());
+// app.use(cors());
 
-app.options("*", cors());
+// app.options("*", cors());
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
