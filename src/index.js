d26cf2b8327e2bb7b2b0de5f956ce4c8ee8670f1
ANDRUXUIS <abdealex05@gmail.com>
2021-01-11 20:45:05 +0100

    Added to deploy to heroku

--- src/index.js
+++ src/index.js
@@ -1,37 +1,16 @@
 const express = require("express");
 const nodemailer = require("nodemailer");
-// const cors = require("cors");
+const cors = require("cors");
+require("dotenv").config();
+
 
 const app = express();
-// app.use(cors());
-
-// app.options("*", cors());
 app.use(express.json());
-
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:8000");
-
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", false);
-
-  // Pass to next layer of middleware
-  next();
-});
-
+app.use(
+  cors({
+    origin: process.env.HOST_SITE,
+  })
+);
 app.post("/", (req, res) => {
   // create reusable transporter object using the default SMTP transport
   const transporter = nodemailer.createTransport({
