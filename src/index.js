c5c870fd8e5ae864149340a60d1c34845f1cabaa
ANDRUXUIS <abdealex05@gmail.com>
2021-01-11 21:53:59 +0100

    Added to deploy to heroku

--- src/index.js
+++ src/index.js
@@ -7,6 +7,22 @@ require("dotenv").config();
 const app = express();
 app.use(express.json());
 app.use(cors());
+var corsMiddleware = function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:8000"); //replace localhost with actual host
+  res.header(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, PUT, PATCH, POST, DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, X-Requested-With, Authorization"
+  );
+
+  next();
+};
+
+app.use(corsMiddleware);
+
 
 
 app.post("/", (req, res) => {
